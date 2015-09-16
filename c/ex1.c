4156f70f7a1a6458dff28c8db7fe46fe572c0740
Hao.Liang <Eric@lianghaodeMBP.lan>
2015-09-17 01:04:22 +0800

    Add c directory.
    Signed-off-by: Hao.Liang <Eric@lianghaodeMBP.lan>

---
+++ c/ex1.c
@@ -0,0 +1,12 @@
+// difference bewteen puts and printf ?
+// puts无法格式化，并且会添加\n
+// argv parameter format ?
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	puts("Hello world");
+	printf("Hello world\n");
+	return 0;
+}
