7360fd6e076a7b38526c0a0004cdaa30bdb4acf1
Hao.Liang <hao.c.code@gmail.com>
2016-06-28 00:12:10 +0800

    Delete redundant files
    Signed-off-by: Hao.Liang <hao.c.code@gmail.com>

--- Linux/driver/Exercise/.class.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hao/Workspace/my_project/Learn/Linux/driver/Exercise/class.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hao/Workspace/my_project/Learn/Linux/driver/Exercise/class.ko /home/hao/Workspace/my_project/Learn/Linux/driver/Exercise/class.o /home/hao/Workspace/my_project/Learn/Linux/driver/Exercise/class.mod.o
