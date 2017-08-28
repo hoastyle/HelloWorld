d3891bc541273897be32a0f00f514ccc1dd7e649
Hao Liang <hao.c.code@gmail.com>
2017-08-28 17:02:30 +0800

    Re-org FSM QP Practice folder
    - Move kbhit function to kbhit.h
    
    Signed-off-by: Hao Liang <hao.c.code@gmail.com>

--- FSM/QP_Practice/bomb/bomb_table.c
+++ FSM/QP_Practice/bomb/bomb_table.c
@@ -4,42 +4,14 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-
-/* for kbhit */
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-
 #include <assert.h>
+#include "../include/kbhit.h"
 
 #define INIT_TIMEOUT 10
 
 #define TRAN(bomb, target) (bomb->curState = target)
 
 void bomb_boom();
-
-int kbhit(void)
-{
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-	if (ch != EOF) {
-		ungetc(ch, stdin);
-		return 1;
-	}
-	return 0;
-}
 
 /* Signal */
 typedef enum {
