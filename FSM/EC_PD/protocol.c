74a6a5a0b44907fb93b7c134cc6a80f566b06cb7
Hao Liang <hao.c.code@gmail.com>
2017-09-01 18:20:58 +0800

    Update protocol.c
    Signed-off-by: Hao Liang <hao.c.code@gmail.com>

--- FSM/EC_PD/protocol.c
+++ FSM/EC_PD/protocol.c
@@ -1,3 +1,13 @@
+#define PD_CAPS_COUNT 50
+
+enum state {
+
+};
+
+static inline void set_state_timeout(int port,    
+                     uint64_t timeout,            
+                     enum pd_states timeout_state);
+
 void pd_state_machine(void)
 {
 	int state;
@@ -40,11 +50,28 @@ void pd_state_machine(void)
 
 			// 在文档中，只需要超时即可，在ec中，需要等待一段时间使Power稳定，直到稳定进入discovery
 			// 没有实现swap的部分
-			set_state_timeout(port, get_time().val + PD_POWER_SUPPLY_TURN_ON_DELAY, PD_STATE_SRC_DISCOVERY);
+			set_state_timeout(port, get_time().val + PD_POWER_SUPPLY_TURN_ON_DELAY, PD_STATE_SRC_SEND_CAPABILITIES);
 			
 			break;
 		case PD_SRC_SEND_CAPABILITIES:
-
+		/*
+		 * summary: send pd capabilities message
+		 * enter: send message, increment caps_count
+		 * exit: 
+		 * event: GoodCRC received, send failed, timeout
+		 */
+			if (caps_count < PD_CAPS_COUNT) {
+				ret = send_source_cap(port);
+				if (ret >= 0) {
+					set_state(port, PD_STATE_SRC_NEGOTIATE_CAPABILITY);
+					timeout = 10 * MSEC;
+					caps_count = 0;
+					// 用来当达到次数时进入disabled
+					hard_reset_count = 0;
+				} else {
+					
+				}
+			}
 			break;
 		case PD_STATE_SRC_DISCOVERY:
 
@@ -64,3 +91,11 @@ void pd_state_machine(void)
 		}
 	}
 }
+
+static inline void set_state_timeout(int port,    
+                     uint64_t timeout,            
+                     enum pd_states timeout_state)
+{                                                 
+    pd[port].timeout = timeout;                   
+    pd[port].timeout_state = timeout_state;       
+}                                                 
